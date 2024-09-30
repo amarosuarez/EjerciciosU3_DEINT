ca01e6291ea09556829face33b0ac7912cc6fbde
amarosuarez <amaro.suarez@iesnervion.es>
2024-09-30 14:39:28 +0200

    Agregar archivos de proyecto.

---
+++ BibliotecaDeClases/Persona.cs
@@ -0,0 +1,30 @@
+﻿namespace BibliotecaDeClases
+{
+    public class Persona
+    {
+        private String apellido;
+        private String nombre;
+        private DateTime fechaNacimiento;
+        public String Apellido
+        {
+            get { return apellido; }
+            set { apellido = value.ToUpper(); }
+        }
+        public String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value.ToLower(); }
+        }
+        public DateTime FechaNac
+        {
+            get { return fechaNacimiento; }
+            set
+            {
+                if (value.Year >= 1900)
+                {
+                    fechaNacimiento = value;
+                }
+            }
+        }
+    }
+}
