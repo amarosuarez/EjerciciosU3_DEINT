a27d74d354ed35ce2736d8e2bc341d351671635b
amarosuarez <amaro.suarez@iesnervion.es>
2024-10-04 01:17:26 +0200

    Ejercicios hechos

--- WinFormsHola/Form1.Designer.vb
+++ WinFormsHola/Form1.Designer.vb
@@ -22,16 +22,140 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Button1 = New Button()
+        tbNombre = New TextBox()
+        tbApellido = New TextBox()
+        tbDia = New TextBox()
+        tbMes = New TextBox()
+        tbAño = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
         SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(363, 245)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' tbNombre
+        ' 
+        tbNombre.Location = New Point(350, 70)
+        tbNombre.Name = "tbNombre"
+        tbNombre.Size = New Size(100, 23)
+        tbNombre.TabIndex = 1
+        ' 
+        ' tbApellido
+        ' 
+        tbApellido.Location = New Point(350, 127)
+        tbApellido.Name = "tbApellido"
+        tbApellido.Size = New Size(100, 23)
+        tbApellido.TabIndex = 2
+        ' 
+        ' tbDia
+        ' 
+        tbDia.Location = New Point(281, 189)
+        tbDia.Name = "tbDia"
+        tbDia.Size = New Size(47, 23)
+        tbDia.TabIndex = 3
+        ' 
+        ' tbMes
+        ' 
+        tbMes.Location = New Point(376, 189)
+        tbMes.Name = "tbMes"
+        tbMes.Size = New Size(47, 23)
+        tbMes.TabIndex = 4
+        ' 
+        ' tbAño
+        ' 
+        tbAño.Location = New Point(466, 189)
+        tbAño.Name = "tbAño"
+        tbAño.Size = New Size(47, 23)
+        tbAño.TabIndex = 5
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(350, 52)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(51, 15)
+        Label1.TabIndex = 6
+        Label1.Text = "Nombre"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(350, 109)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(56, 15)
+        Label2.TabIndex = 7
+        Label2.Text = "Apellidos"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(281, 171)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(24, 15)
+        Label3.TabIndex = 8
+        Label3.Text = "Día"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(377, 171)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(29, 15)
+        Label4.TabIndex = 9
+        Label4.Text = "Mes"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(466, 171)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(29, 15)
+        Label5.TabIndex = 10
+        Label5.Text = "Año"
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(tbAño)
+        Controls.Add(tbMes)
+        Controls.Add(tbDia)
+        Controls.Add(tbApellido)
+        Controls.Add(tbNombre)
+        Controls.Add(Button1)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents tbNombre As TextBox
+    Friend WithEvents tbApellido As TextBox
+    Friend WithEvents tbDia As TextBox
+    Friend WithEvents tbMes As TextBox
+    Friend WithEvents tbAño As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
 
 End Class
--- WinFormsHola/Form1.vb
+++ WinFormsHola/Form1.vb
@@ -1,7 +1,91 @@
-﻿Imports BibliotecaDeClases
+﻿Imports System.Numerics
+Imports System.Runtime.InteropServices.JavaScript.JSType
+Imports System.Security.Cryptography
+Imports BibliotecaDeClases
 
 Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim persona As New Persona
     End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim nombre As String = tbNombre.Text
+        Dim apellidos As String = tbApellido.Text
+        Dim dia As String = tbDia.Text
+        Dim mes As String = tbMes.Text
+        Dim año As String = tbAño.Text
+        Dim correct As Boolean = False
+
+        If nombre = "" Or apellidos = "" Or dia = "" Or mes = "" Or año = "" Then
+            MessageBox.Show("Por favor, rellena todos los campos.", "Campos vacíos", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        Else
+            Dim iDay As Integer = Integer.Parse(dia)
+            Dim iMonth As Integer = Integer.Parse(mes)
+            Dim iYear As Integer = Integer.Parse(año)
+
+            Select Case mes
+                Case 1, 3, 5, 7, 8, 10, 12
+                    If iDay > 0 And iDay <= 31 Then
+
+                        correct = True
+
+                    End If
+                Case 4, 6, 9, 11
+                    If iDay > 0 And iDay < 31 Then
+                        correct = True
+                    End If
+                Case 2
+                    If DateTime.IsLeapYear(iYear) Then
+                        If iDay > 0 And iDay <= 29 Then
+                            correct = True
+                        End If
+                    Else
+                        If iDay > 0 And iDay < 29 Then
+                            correct = True
+                        End If
+                    End If
+
+            End Select
+
+        End If
+
+        If correct Then
+            Dim cumple As DateTime
+
+            Dim fecha As String = dia + "/" + mes + "/" + año
+
+            cumple = DateTime.Parse(fecha)
+
+            Dim persona As New Persona(nombre, apellidos, cumple)
+            Dim Msg, Style, Title, Help, Ctxt, Response
+
+            Msg = "Naciste el " + persona.FechaNac.ToLongDateString
+
+            ' Define message.
+            Style = vbDefaultButton2    ' Define buttons.
+            Title = "Hola, " + persona.Nombre + " " + persona.Apellido    ' Define title.
+            Help = "DEMO.HLP"    ' Define Help file.
+            Ctxt = 1000    ' Define topic context. 
+            ' Display message.
+            Response = MsgBox(Msg, Style, Title)
+
+            ' Opcionalmente, puedes manejar la respuesta del usuario
+            If Response = vbYes Then
+                ' Código para continuar
+            Else
+                ' Código si se selecciona "No"
+            End If
+        Else
+            MessageBox.Show("Por favor, comprueba los campos y la fecha.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+
+
+    End Sub
+
+    Private Sub TextBoxDia_KeyPress(sender As Object, e As KeyPressEventArgs) Handles tbDia.KeyPress, tbMes.KeyPress, tbAño.KeyPress
+        ' Solo permite números y la tecla de retroceso (Backspace)
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not e.KeyChar = ChrW(Keys.Back) Then
+            e.Handled = True ' Cancela la tecla si no es un número
+        End If
+    End Sub
+
 End Class
